7a39f919d1efd1d8a0f7767964adaa3dd044b56a
faiz1 <faiz1@NaviraFaisel.attlocal.net>
2018-12-23 09:06:42 -0500

    git commit -a

--- faisel/src/faisel/faisel.java
+++ faisel/src/faisel/faisel.java
@@ -4,7 +4,9 @@ public class faisel {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("trying git");
+		System.out.println("trying git"); 
+		System.out.println("Getting there");
+		//Faisel
 	}
 
 }
